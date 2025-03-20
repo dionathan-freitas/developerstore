55acbe2acd77bec2e6e840a3bd0dc8ccbf95b613
Dionatha Freitas <dionathan.cs.freitas@gmail.com>
2025-03-20 11:59:19 -0300

    Estruturando repositório - backend na pasta API

---
+++ Api/src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Sale : BaseEntity
+    {
+        public string SaleNumber { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Guid CustomerId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Guid BranchId { get; set; }
+        public List<SaleItem> Items { get; set; } = new();
+        public bool IsCancelled { get; set; }
+    }
+}
---
+++ Api/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.SaleNumber)
+                   .IsRequired()
+                   .HasMaxLength(20);
+
+            builder.Property(s => s.SaleDate)
+                   .IsRequired();
+
+            builder.Property(s => s.TotalAmount)
+                   .HasColumnType("decimal(18,2)");
+
+            builder.Property(s => s.IsCancelled)
+                   .HasDefaultValue(false);
+
+            builder.HasMany(s => s.Items)
+                   .WithOne()
+                   .HasForeignKey(i => i.SaleId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
